ea61fccdad96739e44ceb896109a74f8172b1522
Rishik Rastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-10-04 07:52:27 +0530

    Initial bliss tree

--- bliss_Z01R.mk
+++ bliss_Z01R.mk
@@ -23,13 +23,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/asus/Z01R/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := derp_Z01R
+PRODUCT_NAME := bliss_Z01R
+
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
