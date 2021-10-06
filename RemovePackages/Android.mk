0b49efed5d526dee6392895a3db63eac21e45b49
yograjsingh-cmd <yograjfire18@gmail.com>
2021-10-05 21:54:17 -0500

    Add more remove packages from package

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Drive Maps MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt PrebuiltGmail GoogleFeedback Videos Ornament Photos Tycho GoogleFeedback WellbeingPrebuilt SafetyHubPrebuilt FilesPrebuilt LatinIMEGooglePrebuilt SoundAmplifierPrebuilt PrebuiltBugle
+LOCAL_OVERRIDES_PACKAGES := Drive Maps MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt PrebuiltGmail GoogleFeedback Videos Ornament Photos Tycho GoogleFeedback WellbeingPrebuilt SafetyHubPrebuilt FilesPrebuilt LatinIMEGooglePrebuilt SoundAmplifierPrebuilt PrebuiltBugle CameraRoll
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
