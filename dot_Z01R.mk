e4dd0393caf6543e84f3524db8259152a5c8288c
Rishik Rastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-10-09 17:58:03 +0530

    Fix for dot

--- dot_Z01R.mk
+++ dot_Z01R.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/asus/Z01R/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 DOT_BUILD_TYPE := UNOFFICIAL
 
